57b03b22e3f4dab8a63f87581a9727e9eadb0555
Valentina Valverde <vvgreenville@gmail.com>
2024-03-13 11:42:50 -0400

    Created "Who am I?" for home page. Still need to replace smiley icon with my logo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "./components/LandingPage";
 import FeaturedWork from "./components/FeaturedWork";
+import Who from "./components/Who";
 
 function App() {
   return (
     <>
       <LandingPage />
       <FeaturedWork />
+      <Who />
     </>
   );
 }
---
+++ src/components/Who.jsx
@@ -0,0 +1,55 @@
+export default function Who() {
+  return (
+    <>
+      <div className="who-container">
+        <div className="half">
+          <p className="who">who am i?</p>
+          <p className="am-i">
+            Hi! My name is Valentina and I am an 18-year-old web developer with
+            an eye for UI/UX design. Though I prefer working with the frontend,
+            I am experienced in fullstack applications. I enjoy creating
+            appealing, functional websites that are responsive and user
+            friendly. Please feel free to reach out with project opportunities!
+            I love sharing ideas and meeting new people :)
+          </p>
+        </div>
+        <div className="half logo">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="white"
+            className="bi bi-emoji-smile"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+            <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5" />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+}
+
+{
+  /* <p>
+  Hi, I’m Val! I’m an 18-year-old web developer with an eye for UI/UX design. I
+  currently work out of a coworking space that gives me the opportunity to talk
+  with really interesting and talented people. I attained this job through an
+  apprenticeship program, and I am given prompts each week to complete. I also
+  like to write about technical challenges I may face, or even just personal
+  thoughts in the workplace. If you’d like to know more about my technical side,
+  be sure to checkout my technical portfolio! Otherwise, you can checkout my
+  Github or connect with me on LinkedIn. Thanks!
+</p>; */
+}
+
+{
+  /* <p>
+  Hi there! My name is Valentina and I am a recent graduate of Carolina Code
+  School. Though I prefer working with the frontend, I am experienced in
+  fullstack applications. I have a strong appreciation for UI/UX design and
+  enjoy making appealing, functional websites that are responsive and user
+  friendly.
+</p>; */
+}
