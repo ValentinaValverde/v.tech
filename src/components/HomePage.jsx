5e1e236d2e79433a37d4c782f5a465658c5b0506
Valentina Valverde <vvgreenville@gmail.com>
2024-03-13 09:32:01 -0400

    Attempted to add a home page, we'll see if it actually works.

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <>
+      <p>this is my home page</p>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
     <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
