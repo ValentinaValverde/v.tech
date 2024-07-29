9150f62e0ce6d0487e6e0b9e5a4e5fde7d727627
Valentina Valverde <vvgreenville@gmail.com>
2024-07-29 17:29:30 -0400

    Changed level

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,7 @@ export default function LandingPage() {
   return (
     <>
       <div className="landing">
-        <p>LVL 18</p>
+        <p>LVL 19</p>
         <p id="name">Valentina Valverde</p>
         <p>Web Developer - Greenville, SC</p>
       </div>
